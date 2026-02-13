5da461f8353b92b88435af38b953411906d8eb31
patalena <lenapat0212@gmail.com>
2026-02-13 17:30:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,16 @@
-// Copyright 2021 NNTU-CS
-
+#include <iostream>
+#include "alg.h"
 int gcd(int a, int b) {
-	return 25;
+    int i;
+    while (b != 0) {
+        i = b;
+        b = a % b;
+        a = i;
+    }
+    return a;
+}
+
+int main() {
+    std::cout << gcd(10, 15) << std::endl;
+    return 0;
 }
